4b4f14bfb1ec0d007c7960727d0e3b4ffef0da99
sejung1218 <sejung1218@hanmail.net>
2023-06-16 15:01:53 +0900

    에러코드 주석처리

--- src/common/httpClient.ts
+++ src/common/httpClient.ts
@@ -1,102 +1,122 @@
-import Axios, { AxiosError, AxiosRequestConfig, Method } from 'axios';
-import { jwtRequestInterceptor } from '@/common/httpInterceptor/jwtRequestInterceptor';
-import { responseInterceptor } from '@/common/httpInterceptor';
-import { tokenInterceptor } from '@/common/httpInterceptor/tokenInterceptor';
-import * as process from 'process';
+// // import Axios, { AxiosError, AxiosRequestConfig, Method } from 'axios';
+// // import { jwtRequestInterceptor } from '@/common/httpInterceptor/jwtRequestInterceptor';
+// // import { responseInterceptor } from '@/common/httpInterceptor';
+// // import { tokenInterceptor } from '@/common/httpInterceptor/tokenInterceptor';
+// import * as process from 'process';
 
-/**
- *  * Basic
- *  * Security scheme type:  HTTP
- *  * HTTP Authorization Scheme  basic
- */
+// /**
+//  *  * Basic
+//  *  * Security scheme type:  HTTP
+//  *  * HTTP Authorization Scheme  basic
+//  */
 
-export const axiosHeaders = {
-  // "Api-key":
-  withCredentials: true
-};
-export const axiosSetting = {
-  baseUrl: process.env.BASE_URL,
-  scheme: process.env.NEXT_PUBLIC_SCHEME,
-  host: process.env.NEXT_PUBLIC_HOST,
-  server() {
-    return `${this.scheme}://${this.host}/api/v1`;
-  }
-};
+// export const axiosHeaders = {
+//   // "Api-key":
+//   withCredentials: true,
+// };
+// export const axiosSetting = {
+//   baseUrl: process.env.BASE_URL,
+//   scheme: process.env.NEXT_PUBLIC_SCHEME,
+//   host: process.env.NEXT_PUBLIC_HOST,
+//   server() {
+//     return `${this.scheme}://${this.host}/api/v1`;
+//   },
+// };
 
-export const api = Axios.create({ baseURL: axiosSetting.server(), headers: axiosHeaders });
-triggerInterceptors();
+// export const api = Axios.create({
+//   baseURL: axiosSetting.server(),
+//   headers: axiosHeaders,
+// });
+// triggerInterceptors();
 
-export const HTTPREQUEST = async <T = any>(url: string, config: AxiosRequestConfig = {}) => {
-  try {
-    const response = await api(url, { ...config });
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
+// export const HTTPREQUEST = async <T = any>(
+//   url: string,
+//   config: AxiosRequestConfig = {}
+// ) => {
+//   try {
+//     const response = await api(url, { ...config });
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
 
-export const GET = async <T = any>(url: string, { params = {}, headers = {} } = {}) => {
-  try {
-    const response = await api.get<T>(url, { params, headers });
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
-export const POST = async <T = any>(url: string, data: object = {}, config: AxiosRequestConfig = {}) => {
-  try {
-    const response = await api.post<T>(url, data, config);
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
+// export const GET = async <T = any>(
+//   url: string,
+//   { params = {}, headers = {} } = {}
+// ) => {
+//   try {
+//     const response = await api.get<T>(url, { params, headers });
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
+// export const POST = async <T = any>(
+//   url: string,
+//   data: object = {},
+//   config: AxiosRequestConfig = {}
+// ) => {
+//   try {
+//     const response = await api.post<T>(url, data, config);
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
 
-//CodeCompile을 위한 전용 함수
-export const POST_FOR_CODE = async <T = any>(
-  url: string,
-  data: string | object = {},
-  config: AxiosRequestConfig = {}
-) => {
-  try {
-    const response = await api.post<T>(url, data, config);
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
+// //CodeCompile을 위한 전용 함수
+// export const POST_FOR_CODE = async <T = any>(
+//   url: string,
+//   data: string | object = {},
+//   config: AxiosRequestConfig = {}
+// ) => {
+//   try {
+//     const response = await api.post<T>(url, data, config);
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
 
-export const PUT = async <T = any>(url: string, data: object = {}, { params = {}, headers = {} } = {}) => {
-  try {
-    const response = await api.put<T>(url, data, { params, headers });
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
+// export const PUT = async <T = any>(
+//   url: string,
+//   data: object = {},
+//   { params = {}, headers = {} } = {}
+// ) => {
+//   try {
+//     const response = await api.put<T>(url, data, { params, headers });
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
 
-export const DELETE = async (url: string, { params = {}, headers = {} } = {}) => {
-  try {
-    const response = await api.delete(url, { params, headers });
-    return response.data;
-  } catch (err: any) {
-    return handleError(err);
-  }
-};
+// export const DELETE = async (
+//   url: string,
+//   { params = {}, headers = {} } = {}
+// ) => {
+//   try {
+//     const response = await api.delete(url, { params, headers });
+//     return response.data;
+//   } catch (err: any) {
+//     return handleError(err);
+//   }
+// };
 
-const handleError = (err: AxiosError | any) => {
-  if (err.response) {
-    throw err.response;
-  }
+// const handleError = (err: AxiosError | any) => {
+//   if (err.response) {
+//     throw err.response;
+//   }
 
-  throw err;
-};
+//   throw err;
+// };
 
-function triggerInterceptors() {
-  // request interceptors
-  jwtRequestInterceptor();
+// function triggerInterceptors() {
+//   // request interceptors
+//   jwtRequestInterceptor();
 
-  // response interceptors
-  responseInterceptor();
-  tokenInterceptor();
-}
+//   // response interceptors
+//   responseInterceptor();
+//   tokenInterceptor();
+// }
--- src/layouts/List/ListManagement.tsx
+++ src/layouts/List/ListManagement.tsx
@@ -1,7 +1,7 @@
 import { CustomTable } from '@/components/ui/CustomTable';
 import { useState } from 'react';
-import {HeadRow} from "@/components/ui/CustomTable/CustomTable";
-import { getUserPostList } from '@common/apis/list';
+import { HeadRow } from '@/components/ui/CustomTable/CustomTable';
+// import { getUserPostList } from '@common/apis/list';
 // import { HeadRow } from '@components/ui/CustomTable/CustomTable';
 import * as customTableStyle from '@/components/ui/CustomTable/customTableStyle';
 
@@ -11,7 +11,7 @@ const headRows: HeadRow[] = [
   { name: '등록자', align: 'center', width: '20%', value: 'user.nickName' },
   { name: '작성일', align: 'center', width: '10%', value: 'createdDtime' },
   { name: '수정일', align: 'center', width: '10%', value: 'modifiedDtime' },
-  { name: '공지여부', align: 'center', width: '10%', value: 'noticeYn' }
+  { name: '공지여부', align: 'center', width: '10%', value: 'noticeYn' },
 ];
 export function ListManagement() {
   const [page, setPage] = useState<number>(1);
@@ -21,30 +21,43 @@ export function ListManagement() {
   };
 
   // 공지사항 전체 리스트 API
-  const {
-    data: postListData,
-    isLoading: postListIsLoading,
-    error: postListError
-  } = getUserPostList({ postType: 'TYPE_NOTICE', courseSeq: 0, page: page, elementCnt: 10 });
-  const postListDataContent = postListData?.content;
+  // const {
+  //   data: postListData,
+  //   isLoading: postListIsLoading,
+  //   error: postListError,
+  // } = getUserPostList({
+  //   postType: 'TYPE_NOTICE',
+  //   courseSeq: 0,
+  //   page: page,
+  //   elementCnt: 10,
+  // });
+  // const postListDataContent = postListData?.content;
 
-  if (postListIsLoading) {
-    return <customTableStyle.ManagementContainer>Loading...</customTableStyle.ManagementContainer>;
-  }
-  if (postListError) {
-    return <customTableStyle.ManagementContainer>Error...</customTableStyle.ManagementContainer>;
+  // if (postListIsLoading) {
+  //   return (
+  //     <customTableStyle.ManagementContainer>
+  //       Loading...
+  //     </customTableStyle.ManagementContainer>
+  //   );
+  // }
+  // if (postListError) {
+  //   return (
+  //     <customTableStyle.ManagementContainer>
+  //       Error...
+  //     </customTableStyle.ManagementContainer>
+  //   );
   // }
   return (
     <customTableStyle.ManagementContainer>
-      <CustomTable
-        adminCenterTitleKr={'공지사항'}
-        adminCenterTitleEng={'notice'}
-        headRows={headRows}
-        data={postListDataContent}
-        count={postListData.totalPages}
-        page={page}
-        onPageChange={handlePageChange}
-      />
+      {/* <CustomTable
+          adminCenterTitleKr={'공지사항'}
+          adminCenterTitleEng={'notice'}
+          headRows={headRows}
+          data={postListDataContent}
+          count={postListData.totalPages}
+          page={page}
+          onPageChange={handlePageChange}
+        /> */}
     </customTableStyle.ManagementContainer>
   );
 }
