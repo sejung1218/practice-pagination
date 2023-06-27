920a00e9065a1a2cff91fb1aeb44d21370dee982
sejung1218 <sejung1218@hanmail.net>
2023-06-27 17:31:18 +0900

    충남 LMS 게시판 호출 완료

---
+++ src/common/apis/CnLmsBoardList.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+// [유저] 공지사항 및 자료실 전체 리스트 API
+export const getUserCnLmsPostList = ({
+  boardType,
+  page,
+  elementCnt,
+}: {
+  boardType: string;
+  page: number;
+  elementCnt?: number;
+}) => {
+  return useQuery(
+    ['postListData', { boardType, page, elementCnt }],
+    async () => {
+      const response = await axios.get(`https://api.bonobono.dev/api/v1/post`, {
+        // headers: { Authorization: `Bearer ${accessToken}` },
+        params: { boardType, page, elementCnt },
+      });
+      return response.data;
+    }
+  );
+};
+
+// [유저] 공지사항 및 자료실 상세 보기 API
+export const getUserPostDetail = ({ postSeq }: { postSeq: number | null }) => {
+  return useQuery(
+    ['postDetailData', { postSeq }],
+    async () => {
+      const response = await axios.get(
+        `https://lcapidev.bonobono.dev/api/v1/post/${postSeq}`,
+        {
+          params: { postSeq },
+        }
+      );
+      return response.data;
+    },
+    {
+      enabled: postSeq !== null && postSeq !== undefined, // postSeq가 null이나 undefined가 아닐 때만 API 호출
+      refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리패치 방지
+    }
+  );
+};
--- src/layouts/Step01/StepManagement.tsx
+++ src/layouts/Step01/StepManagement.tsx
@@ -1,8 +1,64 @@
+import { getUserCnLmsPostList } from '@/common/apis/CnLmsBoardList';
+import { HeadRow } from '@/components/ui/CustomTable/CustomTable';
+import { useState } from 'react';
+import { CustomTable } from '@/components/ui/CustomTable';
+import * as customTableStyle from '@/components/ui/CustomTable/customTableStyle';
+
+const headRows: HeadRow[] = [
+  { name: 'No', align: 'center', width: '5%', value: 'seq' },
+  { name: '제목', align: 'center', width: '45%', value: 'subject' },
+  { name: '등록자', align: 'center', width: '20%', value: 'user.nickName' },
+  { name: '작성일', align: 'center', width: '10%', value: 'createdDtime' },
+  { name: '수정일', align: 'center', width: '10%', value: 'modifiedDtime' },
+  { name: '공지여부', align: 'center', width: '10%', value: 'noticeYn' },
+];
+
 export function StepManagement() {
+  const [page, setPage] = useState<number>(1);
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  // 공지사항 전체 리스트 API
+  const {
+    data: postListData,
+    isLoading: postListIsLoading,
+    error: postListError,
+  } = getUserCnLmsPostList({
+    boardType: 'TYPE_NOTICE',
+    page: page,
+    // elementCnt: 10,
+  });
+  const postListDataContent = postListData?.data.content;
+
+  console.log('데이터 : ', postListDataContent);
+  if (postListIsLoading) {
+    return (
+      <customTableStyle.ManagementContainer>
+        Loading...
+      </customTableStyle.ManagementContainer>
+    );
+  }
+  if (postListError) {
+    return (
+      <customTableStyle.ManagementContainer>
+        Error...
+      </customTableStyle.ManagementContainer>
+    );
+  }
   return (
-    <>
-      <div>하이</div>
-      <div>마트</div>
-    </>
+    // <customTableStyle.ManagementContainer>
+    //   <CustomTable
+    //     adminCenterTitleKr={'공지사항'}
+    //     adminCenterTitleEng={'notice'}
+    //     headRows={headRows}
+    //     data={postListDataContent}
+    //     count={postListData.data.totalPages}
+    //     page={page}
+    //     onPageChange={handlePageChange}
+    //   />
+    // </customTableStyle.ManagementContainer>
+    <div>테이블 수정중</div>
   );
 }
